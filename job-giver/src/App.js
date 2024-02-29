320ecab8ea28604d8726e3ec8144040571671ae0
stranger-creator <raosuhas0@gmail.com>
2024-02-29 14:39:34 +0530

    Login frontend

--- job-giver/src/App.js
+++ job-giver/src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
+import "./App.css";
+import { Routes, Route, Navigate } from "react-router-dom";
+import SignUp from "./components/SignUp";
+import Login from "./components/Login";
 
 function App() {
-  return (
-    <div className="App">
-        <h1>Hello</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="signup" element={<SignUp />} />
+                <Route path="login" element={<Login />} />
+                <Route path="/" element={<Navigate to="/signup" />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
